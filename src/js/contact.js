4fc4151c69423e013c0fee1fda3fd7a707f85bcb
Danila Kikeev <kikeevs25@mail.ru>
2025-07-02 00:22:02 +0300

    Created contact section

---
+++ src/js/contact.js
@@ -0,0 +1,10 @@
+import '../assets/scss/contact.scss';
+
+//component
+import { useTheme } from './components/theme.js';
+
+useTheme();
+//Burger
+
+import { useBurger } from './components/burger.js';
+useBurger();
