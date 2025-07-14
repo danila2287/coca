8a7793e66873861ca6ba5054f3bd55899451ff53
Danila Kikeev <kikeevs25@mail.ru>
2025-07-14 18:58:49 +0300

    Created detals-blog

---
+++ src/js/details.js
@@ -0,0 +1,10 @@
+import '/scss/details.scss';
+
+//component
+import { useTheme } from './components/theme.js';
+
+useTheme();
+//Burger
+
+import { useBurger } from './components/burger.js';
+useBurger();
